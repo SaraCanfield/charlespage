3b13ab3c336d0e83dd32fd783efbe8fc4bb12a16
Sara Canfield <slcanfield1@verizon.net>
2015-12-19 17:01:27 -0500

    initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,54 @@
+class PostsController < ApplicationController
+
+	def index
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new post_params
+
+		if @post.save
+				redirect_to @post, notice: "Post Saved"
+		else
+			render 'new', notice: "error saving post"
+		end
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		if @post.update post_params
+			redirect to @post, notice: "Post update"
+		else
+
+			render 'edit', notice: "post was not updated"
+		end
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to posts_path
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+
+	private
+
+
+	def post_params
+		params.require(:post).permit(:title, :content)
+
+	end
+
+
+
+
+end
